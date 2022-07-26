4965c21f297b5d1a2b6211c9cd04ac296b8abbd8
Emmanuel Paul <emmapaul247@gmail.com>
2022-07-26 14:43:49 +0100

    Add invoices table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -12,3 +12,28 @@ create table patients (
     name varchar(80),
     date_of_birth date
 );
+
+-----Create table named invoices----
+CREATE TABLE invoices (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    total_amount decimal,
+    generated_at timestamp,
+    payed_at timestamp,
+    medical_histories_id INT,
+    FOREIGN KEY (medical_histories_id) REFERENCES medical_histories(id)
+    ON UPDATE CASCADE
+);
+
+----Create table named invoices_items-----
+CREATE TABLE invoices_items(
+    id INT PRIMARY KEY ALWAYS GENERATED AS IDENTITY,
+    unit_price decimal,
+    quantity INT,
+    total_price decimal,
+    invoice_id INT,
+    treatement_id INT,
+    FOREIGN KEY (invoice_id) REFERENCES invoice(id)
+    ON UPDATE CASCADE,
+    FOREIGN KEY (treatement_id) REFERENCES treatements(id)
+    ON UPDATE CASCADE
+);
